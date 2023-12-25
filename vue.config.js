949ef646f961fdc8c62ef5c669bf1226ca7a9f88
asadahimeka <smallstar9637@yahoo.co.jp>
2023-12-25 22:08:27 +0800

    chore: update config

--- vue.config.js
+++ vue.config.js
@@ -131,7 +131,7 @@ module.exports = {
     workboxOptions: {
       skipWaiting: true,
       clientsClaim: true,
-      exclude: [/\.map$/, /^manifest.*\.js$/, /lang-.*-json\..*\.js$/],
+      exclude: [/\.map$/, /^manifest.*\.js$/, /lang-.*-json\..*\.js$/, /img\/gbf\/.*\.png/],
       navigateFallbackDenylist: [/^\/prks\//],
       runtimeCaching: [
         {
